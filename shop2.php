d7d6d0a0eda4d09a2b6fa868c137ff4e4b6ae4b7
VianneyPham <gd3451er@go.minnstate.edu>
2022-08-01 22:18:36 -0500

    iframe example

---
+++ shop2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <body>
+    
+    <?php include('header.php'); 
+    ?>
+
+    <iframe src="https://www.google.com/webhp?igu=1" style="height:1080px;width:1920px" title="googlesoc6_iframe"></iframe>
+    <iframe src="https://society6.com/abcd_project/all" style="height:1080px;width:1920px" title="soc6_iframe"></iframe>
+    <iframe src="images\resources\abcd_project_Society6.html" style="height:1080px;width:1920px" title="soc6_iframe"></iframe>
+    
+    </script>
+    <script type=”text/javascript” src=”https://society6.com/abcd_project/show_banner.js”>
+    </script>
+
+    </body>
+</html>
+
